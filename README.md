f594180dbeae19f48e0def5c162199fbb24e3b6e
Mauro-Domingues <maurinho.villa@hotmail.com>
2022-08-16 18:25:21 -0300

    animations-added

---
+++ README.md
@@ -0,0 +1,3 @@
+O site está em construção, então esse layoult e todos os seus elementos são apenas temporários
+
+Visualizar página: https://mauro-domingues.github.io/Mauro-Domingues-portifolio/
---
+++ js/script.js
@@ -0,0 +1,16 @@
+const bioContainer = document.querySelector(".bio-container")
+const bio = document.querySelector(".bio")
+
+let i = 0
+let message = "Sou iniciante na área da programação, recentemente finalizando um curso web full stack com a Startse Tech Academy. Sou apaixonado por tecnologia e uma pessoa extremamente curiosa, sempre querendo aprender mais. #BoraAprender"
+
+setTimeout(() => {
+    bioContainer.style.opacity = "1"
+    function typing(){
+        if (i < message.length){
+            bio.textContent += message.charAt(i)
+            i++
+            setTimeout(typing, 50)
+        }
+    }typing()
+}, 3500)
